e065c30eda9b04335a375d905c1bd5a4b76cdf9b
Aurore Denis <adeni957@gmail.com>
2021-02-19 19:02:23 +0100

    vendredi 19 le début

---
+++ to-do-list-add-item-component/to-do-list-add-item-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-to-do-list-add-item-component',
+  templateUrl: './to-do-list-add-item-component.component.html',
+  styleUrls: ['./to-do-list-add-item-component.component.css']
+})
+export class ToDoListAddItemComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
