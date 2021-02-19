c49846f316e2d7351c5636683df34ffeb80d3396
Aurore Denis <adeni957@gmail.com>
2021-02-19 19:03:15 +0100

    vend redi 19 le début

---
+++ to-do-list-component/to-do-list-component.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-to-do-list-component',
+  templateUrl: './to-do-list-component.component.html',
+  styleUrls: ['./to-do-list-component.component.css']
+})
+export class ToDoListComponentComponent implements OnInit {
+
+  listItem = ['je', 'myself', 'I'];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
