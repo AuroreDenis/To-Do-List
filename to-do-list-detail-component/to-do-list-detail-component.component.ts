522387f624b4c3548bc54a486c3edd1884fcc85b
Aurore Denis <adeni957@gmail.com>
2021-02-19 19:03:34 +0100

    vendredi 19 le début

---
+++ to-do-list-detail-component/to-do-list-detail-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-to-do-list-detail-component',
+  templateUrl: './to-do-list-detail-component.component.html',
+  styleUrls: ['./to-do-list-detail-component.component.css']
+})
+export class ToDoListDetailComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
